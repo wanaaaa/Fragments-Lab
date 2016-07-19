f82e0f91d180d8edeceefd9027f37a3203ffa129
Alan J Caceres <alan.caceres@generalassemb.ly>
2016-07-19 10:10:21 -0400

    Uploading notes and starter-code for Fragments Lab

---
+++ starter-code/app/src/main/java/com/ga/android/myapplication/PlaceholderFragment.java
@@ -0,0 +1,14 @@
+package com.ga.android.myapplication;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by alanjcaceres on 7/19/16.
+ */
+
+public class PlaceholderFragment extends Fragment {
+
+   //This is a blank fragment
+   //Implement your own version of Fragments for this lab
+
+   }
---
+++ starter-code/app/src/main/java/com/ga/android/myapplication/SectionsPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.ga.android.myapplication;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by alanjcaceres on 7/19/16.
+ */
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    int mPageCount;
+
+    public SectionsPagerAdapter(FragmentManager fm, int pageCount) {
+        super(fm);
+        mPageCount = pageCount;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            //Complete this
+            default: return new PlaceholderFragment();
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return mPageCount;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            default:
+            case 0:
+                return "PICTURE";
+            case 1:
+                return "ABOUT ME";
+            case 2:
+                return "CONTACT";
+        }
+
+    }
+}
